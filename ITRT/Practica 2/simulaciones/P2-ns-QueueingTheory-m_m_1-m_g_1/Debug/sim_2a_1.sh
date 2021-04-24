f938bc3ae5132f97282bee3a9738d04ef209ed85
mvTTR <jorge.gomzar@gmail.com>
2021-04-24 12:36:36 +0200

    p2d

--- ITRT/Practica 2/simulaciones/P2-ns-QueueingTheory-m_m_1-m_g_1/Debug/sim_2a_1.sh
+++ ITRT/Practica 2/simulaciones/P2-ns-QueueingTheory-m_m_1-m_g_1/Debug/sim_2a_1.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-P='/home/labs/itrt/itrtx05/scripts2021/ITRT/P2-ns-QueueingTheory-m_m_1-m_g_1/Debug'
+P='/home/ttr/Escritorio/ITRT/teleco2020_21/ITRT/Practica 2/simulaciones/P2-ns-QueueingTheory-m_m_1-m_g_1/Debug'
 
 echo "Simulaciones para M/M/1"
-for i in {1..100}
+for i in {1..500}
 do
-	$P/queue-mm1-mg1 --averaging=1 --fixedlength=0 --tmax=10 --rep=$i >> $P/mm1_10.txt
-	$P/queue-mm1-mg1 --averaging=1 --fixedlength=0 --tmax=100 --rep=$i >> $P/mm1_100.txt
+	./queue-mm1-mg1 --averaging=1 --fixedlength=0 --tmax=10 --rep=$RANDOM >> ./mm1_10.txt
+	./queue-mm1-mg1 --averaging=1 --fixedlength=0 --tmax=100 --rep=$RANDOM >> ./mm1_100.txt
 done
 
 echo "Simulaciones para M/G/1"
-for i in {1..100}
+for i in {1..500}
 do
-	$P/queue-mm1-mg1 --averaging=1 --fixedlength=1 --tmax=10 --rep=$i >> $P/mg1_10.txt
-	$P/queue-mm1-mg1 --averaging=1 --fixedlength=1 --tmax=100 --rep=$i >> $P/mg1_100.txt
+	./queue-mm1-mg1 --averaging=1 --fixedlength=1 --tmax=10 --rep=$RANDOM >> ./mg1_10.txt
+	./queue-mm1-mg1 --averaging=1 --fixedlength=1 --tmax=100 --rep=$RANDOM >> ./mg1_100.txt
 done
--- ITRT/Practica 2/simulaciones/outputs/p2a/p2a_1/sim_2a_1.sh
+++ ITRT/Practica 2/simulaciones/outputs/p2a/p2a_1/sim_2a_1.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-P='../..'
+echo "" > ./mm1_10.txt
+echo "" > ./mm1_10.txt
+echo "" > ./mg1_100.txt
+echo "" > ./mg1_100.txt
 
-echo "Simulaciones para M/M/1"
+P='../..'
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+NORMAL=$(tput sgr0)
+
+printf "${BLUE}Simulaciones M/M/1${NORMAL}\n"
 for i in {1..500}
 do
 	$P/queue-mm1-mg1 --averaging=1 --fixedlength=0 --tmax=10 --rep=$RANDOM >> ./mm1_10.txt
 	$P/queue-mm1-mg1 --averaging=1 --fixedlength=0 --tmax=100 --rep=$RANDOM >> ./mm1_100.txt
 done
 
-echo "Simulaciones para M/G/1"
+printf "${BLUE}Simulaciones M/G/1${NORMAL}\n"
 for i in {1..500}
 do
 	$P/queue-mm1-mg1 --averaging=1 --fixedlength=1 --tmax=10 --rep=$RANDOM >> ./mg1_10.txt
