e6161be0dbc565a3fef779efd0e3dc018bbcbca5
Jorge Gomez Zarzosa <jorge.gomzar@gmail.com>
2021-04-26 21:30:25 +0200

    added line break

--- ITRT/Practica 2/simulaciones/outputs/p2d/check_gauss.m
+++ ITRT/Practica 2/simulaciones/outputs/p2d/check_gauss.m
@@ -3,6 +3,6 @@ clf;
 % cargamos los ficheros con las muestras
 mm1_10000 = load('p2d_mm1/output_Q.csv');
 mg1_10000 = load('p2d_mg1/output_Q.csv');
-printf("Longitud M/M/1: %d", length(mm1_10000));
-printf("Longitud M/G/1: %d", length(mg1_10000));
+printf("Longitud M/M/1: %d\n", length(mm1_10000));
+printf("Longitud M/G/1: %d\n", length(mg1_10000));
 
