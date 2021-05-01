371a56ca849755f69d184c2c879409b9ff1888a3
mvTTR <jorge.gomzar@gmail.com>
2021-05-01 08:53:32 +0200

    updated p2a1

--- ITRT/Practica 2/simulaciones/outputs/p2a/p2a_1/q_hist.m
+++ ITRT/Practica 2/simulaciones/outputs/p2a/p2a_1/q_hist.m
@@ -1,4 +1,5 @@
-clf;
+close all;
+clear;
 
 m10 = load('mm1_10.csv');
 m100 = load('mm1_100.csv');
@@ -8,10 +9,22 @@ g100 = load('mg1_100.csv');
 
 subplot(2,2,1);
 hist(m10,100);
+title('Histograma: Qsim promedio (t=10s) M/M/1');
+xlabel('Frecuencia');
+ylabel('Llegadas a frecuencia X');
 subplot(2,2,2);
 hist(m100,100);
+title('Histograma: Qsim promedio (t=100s) M/M/1');
+xlabel('Frecuencia');
+ylabel('Llegadas a frecuencia X');
 
 subplot(2,2,3);
 hist(g10,100);
+title('Histograma: Qsim promedio (t=10s) M/G/1');
+xlabel('Frecuencia');
+ylabel('Llegadas a frecuencia X');
 subplot(2,2,4);
 hist(g100,100);
+title('Histograma: Qsim promedio (t=100s) M/G/1');
+xlabel('Frecuencia');
+ylabel('Llegadas a frecuencia X');
